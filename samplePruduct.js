85cd65e9d323ec1b6a4a5488a6a08206b92256ee
Jason <sysbender@gmail.com>
2017-03-14 17:27:53 -0400

    sampleProduct.js

---
+++ samplePruduct.js
@@ -0,0 +1,37 @@
+// Some of the Knockout examples use this data
+var sampleProductCategories =[
+  {
+    "products": [
+      {
+        "name": "1950's Chicago Surface Lines Streetcar",
+        "price": 26.72
+      },
+      {
+        "name": "1962 City of Detroit Streetcar",
+        "price": 37.49
+      },
+      {
+        "name": "Collectable Wooden Train",
+        "price": 67.56
+      }
+    ],
+    "name": "Trains"
+  },
+  {
+    "products": [
+      {
+        "name": "1926 Ford Fire Engine",
+        "price": 24.92
+      },
+      {
+        "name": "1940 Ford Pickup Truck",
+        "price": 58.33
+      },
+      {
+        "name": "1940s Ford truck",
+        "price": 84.76
+      }
+    ],
+    "name": "Trucks and Buses"
+  }
+];
